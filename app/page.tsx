30a7d41229fcb8ca5ea661b47efa87bf200e19c9
qver27 <evan.j.lee7@gmail.com>
2025-06-15 19:24:42 -0700

    added image stuff

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@
 import { useState } from 'react';
 import './globals.css';
 import Link from 'next/link';
+import React from "react";
+import lilGuy from "../assets/lilguy.png";
 
 export default function Page() {
   const [list, setList] = useState<string[]>([]);
@@ -13,12 +15,12 @@ export default function Page() {
   };
 
   return (
-    <div>
+    <><div>
       <header>TODOLIFE!!!!</header>
       <p>coolest most awesome project EVER evan candra alice best team EVER let's go</p>
       <Link href="/test">test link</Link>
       <p>List:</p>
-      
+
       <ul>
         {list.map((item) => (
           <li key={item}>{item}</li>
@@ -27,13 +29,14 @@ export default function Page() {
 
       <div>
         <p>Add new item: </p>
-        <input 
-          type="text" 
-          value={input} 
-          onChange={(e) => setInput(e.target.value)} 
-        />
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)} />
         <button type="button" onClick={handleSave}>Save</button>
       </div>
-    </div>
+   </div>
+   <img src={lilGuy.src} alt="Image"></img>
+   </>
   );
 }
