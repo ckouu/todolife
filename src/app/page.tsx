9235c7b577022830c300665837edf3a089095ec2
ckouu <candra.kou@gmail.com>
2025-08-13 10:47:30 -0700

    Chapter interface changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,16 @@
 import Link from 'next/link'
+import Image from 'next/image'
 
 export default function Home() {
 
   return (
-    <div>
+    <div className='home'>
+      <div className='head-container'>
+        todoLIFE
+        <div className='line'>
+          <Image fill src='/line.svg' alt='Line' objectFit='contain'/>
+        </div>
+      </div>
       <Link href='/fitness'>fitness</Link>
       <Link href='/academics'>academics</Link>
     </div>
