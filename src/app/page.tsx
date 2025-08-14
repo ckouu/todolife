d4de8bf07938ceddbb0044878067842ac8981986
ckouu <candra.kou@gmail.com>
2025-08-14 15:18:21 -0700

    Fixed styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import { useState } from 'react'
 
 export default function Home() {
+
+  const [current, setCurrent] = useState('fitness');
+  const chapters =['fitness', 'academics', 'nutrition'];
 
   return (
     <div className='home'>
       <div className='head-container'>
         todoLIFE
         <div className='line'>
-          <Image fill src='/line.svg' alt='Line' objectFit='contain'/>
+          <Image fill src='/line.svg' alt='Line' style={{ objectFit: 'contain' }}/>
         </div>
       </div>
       <Link href='/fitness'>fitness</Link>
